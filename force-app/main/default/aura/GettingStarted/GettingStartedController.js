69038b5c615aa3423731b4eaf7e386330658a292
Mark Hansen <mark@rarekarma.com>
2022-05-11 18:32:00 -0400

    update branch with changes made directly in prod

--- force-app/main/default/aura/GettingStarted/GettingStartedController.js
+++ force-app/main/default/aura/GettingStarted/GettingStartedController.js
@@ -1,72 +1,102 @@
 ({
-    zoomIdPass : function(component, event, helper) {
-        var zoomShow = component.get('v.zoom');	
-        if(zoomShow == 'True'){
-            component.set('v.zoom','False');	
-        }
-        else{
-            component.set('v.zoom','True');	
-        }
-    },
-    
-    canIdPass : function(component, event, helper) {
-        var calShow = component.get('v.calendly');	
-        if(calShow == 'True'){
-            component.set('v.calendly','False');	
-        }
-        else{
-            component.set('v.calendly','True');	
-        }
-    },
-    
-    handleReferralLink : function(component, event, helper) {
-        var action = component.get("c.getReferralLink");
-       
-        action.setCallback(this,function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var resultData = response.getReturnValue();
-                alert('GENERATED URL: '+resultData);
-            }else{
-                var errors = action.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log(errors[0].message);
-                    }
-                }
-                console.log('Error handleReferralLink');
-            }
-        });
-        $A.enqueueAction(action);
-    },
-    
-    getUserAndCohort : function(component) {
-        var action = component.get("c.serverGetUserAndCohort");
-        action.setParams({
-            'programName' : component.get("v.programName").toString()
-        });
-        action.setCallback(this,function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var resultDataString = response.getReturnValue()
-                var resultData = JSON.parse(resultDataString)
-                component.set('v.userId', resultData.userId)
-                component.set('v.cohortId', resultData.cohortId)
-                var aeoStateUnencoded = "{'aeo_cohort_id':'" + resultData.cohortId + "', 'aeo_program_id':'" + component.get('v.programId') + "'}"
-                var aeoStateEncoded = btoa(aeoStateUnencoded)
-                component.set('v.aeo_state', aeoStateEncoded)
-                console.log('aeoStateEncoded:')
-                console.log(aeoStateEncoded)
-                console.log('resultData.userId:')
-                console.log(resultData.userId)
-                console.log('resultData.cohortId:')
-                console.log(resultData.cohortId)
-                console.log('Data: ');
-                console.log(resultData);
-            }else{
-                console.log('Error getUserAndCohort');
-            }
-        });
-        $A.enqueueAction(action);
+  copyResiliToClipboard: function(component, event, helper) {
+    var text = component.find("copyResiliSource").get("v.value");
+    var hiddenInput = document.createElement("input");
+    hiddenInput.setAttribute("value", text);
+    document.body.appendChild(hiddenInput);
+    hiddenInput.select();
+    var didCopyWork = document.execCommand("copy");
+    console.log({ didCopyWork });
+    document.body.removeChild(hiddenInput);
+  },
+  copyPostCoachToClipboard: function(component, event, helper) {
+    var text = component.find("copyPostCoachSource").get("v.value");
+    var hiddenInput = document.createElement("input");
+    hiddenInput.setAttribute("value", text);
+    document.body.appendChild(hiddenInput);
+    hiddenInput.select();
+    var didCopyWork = document.execCommand("copy");
+    console.log({ didCopyWork });
+    document.body.removeChild(hiddenInput);
+  },
+  zoomIdPass: function(component, event, helper) {
+    var zoomShow = component.get("v.zoom");
+    if (zoomShow == "True") {
+      component.set("v.zoom", "False");
+    } else {
+      component.set("v.zoom", "True");
     }
-})
+  },
+
+  canIdPass: function(component, event, helper) {
+    var calShow = component.get("v.calendly");
+    if (calShow == "True") {
+      component.set("v.calendly", "False");
+    } else {
+      component.set("v.calendly", "True");
+    }
+  },
+
+  handleReferralLink: function(component, event, helper) {
+    var action = component.get("c.getReferralLink");
+
+    action.setCallback(this, function(response) {
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        var resultData = response.getReturnValue();
+        alert("GENERATED URL: " + resultData);
+      } else {
+        var errors = action.getError();
+        if (errors) {
+          if (errors[0] && errors[0].message) {
+            console.log(errors[0].message);
+          }
+        }
+        console.log("Error handleReferralLink");
+      }
+    });
+    $A.enqueueAction(action);
+  },
+
+  getUserCohortAndLinks: function(component) {
+    var action = component.get("c.serverGetUserCohortAndLinks");
+    action.setParams({
+      programName: component.get("v.programName").toString()
+    });
+    action.setCallback(this, function(response) {
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        var resultDataString = response.getReturnValue();
+        var resultData = JSON.parse(resultDataString);
+        component.set("v.userId", resultData.userId);
+        component.set("v.cohortId", resultData.cohortId);
+        component.set("v.bitlyResili", resultData.bitlyResili);
+        component.set("v.bitlyPostCoach", resultData.bitlyPostCoach);
+        var aeoStateUnencoded =
+          '{"aeo_cohort_id":"' +
+          resultData.cohortId +
+          '","aeo_program_id":"' +
+          component.get("v.programId") +
+          '"}';
+        var aeoStateEncoded = btoa(aeoStateUnencoded);
+        component.set("v.aeo_state", aeoStateEncoded);
+          
+        console.log("aeoStateEncoded:");
+        console.log(aeoStateEncoded);
+        console.log("resultData.userId:");
+        console.log(resultData.userId);
+        console.log("resultData.cohortId:");
+        console.log(resultData.cohortId);
+        console.log("resultData.bitlyResili:");
+        console.log(resultData.bitlyResili);
+        console.log("resultData.bitlyPostCoach:");
+        console.log(resultData.bitlyPostCoach);
+        console.log("Data: ");
+        console.log(resultData);
+      } else {
+        console.log("Error getUserAndCohort");
+      }
+    });
+    $A.enqueueAction(action);
+  }
+});
--- force-app/main/default/aura/ListView2/ListView2Helper.js
+++ force-app/main/default/aura/ListView2/ListView2Helper.js
@@ -1,18 +1,22 @@
 ({
-    getColumnAndAction : function(component) {
-        var actions = [
-            {label: 'Edit', name: 'edit'},
-            {label: 'Delete', name: 'delete'},
-            {label: 'View', name: 'view'}
-        ];
-        component.set('v.columns', [
-            // {label: 'Program Name', fieldName: 'Program_Name__c', type: 'text'},
-            {label: 'Member Name', fieldName: 'Name__c', type: 'text'},
-            {label: 'Member Email', fieldName: 'Member_Email__c', type: 'text'},
-            {label: 'Member Business Name', fieldName: 'Member_Business_Name__c', type: 'text'},
-            {label: 'Status', fieldName: 'Member_Status__c', type: 'text'},
-            // {label: 'Cohort Name', fieldName: 'Cohort_Name__c', type: 'text'},
-            /* {
+  getColumnAndAction: function (component) {
+    var actions = [
+      { label: "Edit", name: "edit" },
+      { label: "Delete", name: "delete" },
+      { label: "View", name: "view" },
+    ];
+    component.set("v.columns", [
+      // {label: 'Program Name', fieldName: 'Program_Name__c', type: 'text'},
+      { label: "Member Name", fieldName: "Name__c", type: "text" },
+      { label: "Member Email", fieldName: "Member_Email__c", type: "text" },
+      {
+        label: "Member Business Name",
+        fieldName: "Member_Business_Name__c",
+        type: "text",
+      },
+      { label: "Status", fieldName: "Member_Status__c", type: "text" },
+      // {label: 'Cohort Name', fieldName: 'Cohort_Name__c', type: 'text'},
+      /* {
                 label: 'Action',
                 type:  'button',
                 typeAttributes: 
@@ -25,43 +29,44 @@
                     value: 'test'
                 }
             } */
-            //{label: 'AccountNumber', fieldName: 'AccountNumber', type: 'text'},
-            //{label: 'Industry', fieldName: 'Industry', type: 'text'},
-            //{label: 'Phone', fieldName: 'Phone', type: 'phone'},
-            //{type: 'action', typeAttributes: { rowActions: actions } } 
-        ]);
-    },
-     
-    getProgMembers : function(component, helper) {
-        var action = component.get("c.serverGetProgMembers");
-        var pageSize = component.get("v.pageSize").toString();
-        var pageNumber = component.get("v.pageNumber").toString();
-        var programName = component.get("v.programName").toString();
-         
-        action.setParams({
-            'pageSize' : pageSize,
-            'pageNumber' : pageNumber,
-            'programName' : programName
-        });
-        action.setCallback(this,function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var resultData = response.getReturnValue();
-                if(resultData.length < component.get("v.pageSize")){
-                    component.set("v.isLastPage", true);
-                } else{
-                    component.set("v.isLastPage", false);
-                }
-                component.set("v.dataSize", resultData.length);
-                component.set("v.data", resultData);
-                console.log('Data Size: '+resultData.length);
-                console.log('Data: ');
-                console.log(resultData);
-            }else{
-                console.log('Error getProgMembers');
-            }
-        });
-        $A.enqueueAction(action);
-    }
-            
-})
+      //{label: 'AccountNumber', fieldName: 'AccountNumber', type: 'text'},
+      //{label: 'Industry', fieldName: 'Industry', type: 'text'},
+      //{label: 'Phone', fieldName: 'Phone', type: 'phone'},
+      //{type: 'action', typeAttributes: { rowActions: actions } }
+    ]);
+  },
+
+  getProgMembers: function (component, helper) {
+    var action = component.get("c.serverGetProgMembers");
+    var pageSize = component.get("v.pageSize").toString();
+    var pageNumber = component.get("v.pageNumber").toString();
+    var programName = component.get("v.programName").toString();
+      console.log( {programName})
+
+    action.setParams({
+      pageSize: pageSize,
+      pageNumber: pageNumber,
+      programName: programName,
+    });
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        var resultData = response.getReturnValue();
+        console.log({ resultData });
+        if (resultData.length < component.get("v.pageSize")) {
+          component.set("v.isLastPage", true);
+        } else {
+          component.set("v.isLastPage", false);
+        }
+        component.set("v.dataSize", resultData.length);
+        component.set("v.data", resultData);
+        console.log("Data Size: " + resultData.length);
+        console.log("Data: ");
+        console.log(resultData);
+      } else {
+        console.log("Error getProgMembers");
+      }
+    });
+    $A.enqueueAction(action);
+  },
+});
